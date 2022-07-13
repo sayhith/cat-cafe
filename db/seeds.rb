a55936f4c9ff304bbb7df8e185a6ac56880fd2ea
sayhith <adrialwin@gmail.com>
2022-07-13 00:10:47 -0700

    enabled Yarn and added new Products to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.create(name: "Catnip", price: 199, description: "For when your cat needs to chill out.")
+Product.create(name: "Milk", price: 450, description: "Technically you shouldn't be giving this to your cat...")
+Product.create(name: "Chicken", price: 699, description: "A nice hunk of chicken breast.")
